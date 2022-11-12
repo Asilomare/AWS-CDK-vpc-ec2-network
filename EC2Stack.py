1d39a22d0d024092bfe47f96cd5a3b6992ddf898
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-11-12 00:57:57 -0500

    Update EC2Stack.py

--- EC2Stack.py
+++ EC2Stack.py
@@ -7,8 +7,12 @@ class EC2Stack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
+        #change these values accordingly
+        pub_key = "miAKey"
+        priv_key = "miBKey"
+        
         vpc = ec2.Vpc(self, "MyVpc",
-            #nat_gateways=1,
+            nat_gateways=1,
             subnet_configuration=[
                 ec2.SubnetConfiguration(
                     name = 'PublicSN',
@@ -22,12 +26,13 @@ class EC2Stack(cdk.Stack):
                     )
                 ]
             )
-        ANATgw = vpc.public_subnets[0].add_nat_gateway()
-        log_group = aws_logs.LogGroup(self, "log_group")
-        flowlog = ec2.FlowLog(self, "FlowLogs",
-            resource_type=ec2.FlowLogResourceType.from_vpc(vpc),
-            destination=ec2.FlowLogDestination.to_cloud_watch_logs(log_group)
-            )
+            
+        #log groups (for troubleshouting)
+        #log_group = aws_logs.LogGroup(self, "log_group")
+        #flowlog = ec2.FlowLog(self, "FlowLogs",
+        #    resource_type=ec2.FlowLogResourceType.from_vpc(vpc),
+        #    destination=ec2.FlowLogDestination.to_cloud_watch_logs(log_group)
+        #    )
     
         #ec2 instance creation, one associated with public subnet, one associated with private subnet 
         miA = ec2.Instance(self, "miA",
@@ -37,31 +42,27 @@ class EC2Stack(cdk.Stack):
                 subnet_type=ec2.SubnetType.PUBLIC),
             instance_type=ec2.InstanceType(instance_type_identifier="t2.nano"),
             machine_image=ec2.MachineImage.latest_amazon_linux(),
-            key_name="miAKey"#non portable, made separetly in key manager
+            key_name=pub_key
         )
-        miA.connections.allow_from(miA, ec2.Port.tcp(22))
-        miA.connections.allow_to(miA, ec2.Port.tcp(22))    
+        #allows ipv4 connections, to port 22(SSH)
+        miA.connections.allow_from_any_ipv4(ec2.Port.tcp(22))
         
-        #initialize isntances in seperate subnets, not subnet groups
-        miB = ec2.Instance(self, "miB",
+        #private instance
+        miB1 = ec2.Instance(self, "miB1",
             vpc=vpc,
-            instance_name="miB_name",
+            instance_name="miB1_name",
             vpc_subnets=ec2.SubnetSelection(
                 subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
             instance_type=ec2.InstanceType(instance_type_identifier="t2.nano"),
-            machine_image=ec2.MachineImage.latest_amazon_linux()
+            machine_image=ec2.MachineImage.latest_amazon_linux(),
+            key_name=priv_key
             )
+        #allows traffic from public instance
+        miB1.connections.allow_from(miA, ec2.Port.all_traffic())
 
         #routes 
         A_B = ec2.CfnRoute(self, "A_B",
             route_table_id=vpc.public_subnets[0].route_table.route_table_id,
             destination_cidr_block=vpc.isolated_subnets[0].ipv4_cidr_block,
-            instance_id=miB.instance_id
-            )
-        
-        #nat to subnet
-        miAIngress = ec2.CfnRoute(self, "miAIngress",
-            route_table_id=vpc.public_subnets[0].route_table.route_table_id,
-            destination_cidr_block=vpc.public_subnets[0].ipv4_cidr_block,
-            nat_gateway_id=ANATgw.attr_nat_gateway_id
+            instance_id=miB1.instance_id
             )
